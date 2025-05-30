235b4bca1c469c849b21703cc07772caf072397b
aaronharman03 <110352136+aaronharman03@users.noreply.github.com>
2025-05-30 14:26:11 +0100

    Update and rename script.js to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+import { db } from './firebase.js';
+
+// Placeholder until we add Firestore logic
+document.getElementById('add-day').addEventListener('click', () => {
+  alert('Add workout day clicked');
+});
+
