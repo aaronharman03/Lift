12f42f9bd5bdd4d73012e87e2d0f81f5bf1cabfb
aaronharman03 <110352136+aaronharman03@users.noreply.github.com>
2025-05-30 22:58:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,35 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("75days").then(cache => {
-      return cache.addAll(["/", "/index.html", "/manifest.json"]);
+const CACHE_NAME = 'weekly-schedule-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
