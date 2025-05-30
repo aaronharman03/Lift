d38c6c0715a919c154bbb64b6c140c8a30bdc610
aaronharman03 <110352136+aaronharman03@users.noreply.github.com>
2025-05-30 15:31:46 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/styles.css',   // Change if you don’t have this or it’s named differently
+  '/app.js',       // Change if your main JS has a different name
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
